c35bd735e2c29bebba94d3ad149dd4a5b603f7ce
YuHChen <yuhchen21@gmail.com>
2023-08-03 22:17:40 -0400

    Add support for deploying using GitHub Pages.

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-    kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter()
-    }
+  kit: {
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      // See https://kit.svelte.dev/docs/single-page-apps#usage
+      fallback: 'index.html',
+    }),
+    // See https://vitejs.dev/guide/static-deploy.html#github-pages
+    // See https://kit.svelte.dev/docs/adapter-static#github-pages
+    paths: {
+      base: dev ? '' : '/meprpa',
+    },
+  },
 };
 
 export default config;
