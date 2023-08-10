0dc5cfb00fb811c0486bc7061c59bbc9b05e0f5c
YuHChen <yuhchen21@gmail.com>
2023-08-10 00:22:35 -0400

    Fix integration test.

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('index page has expected form', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/meprpa/');
   // await expect(page.getByRole('form')).toBeVisible();
   await expect(
     page.getByRole('button', { name: 'Fetch Spreadsheet', disabled: true }),
